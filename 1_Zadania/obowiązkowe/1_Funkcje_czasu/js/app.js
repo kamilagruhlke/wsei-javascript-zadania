121c33dff9e8507a3d49e926ffb1b89fa03eed0b
Kamila Gruhlke <kamila.gruhlke@microsoft.wsei.edu.pl>
2019-10-06 15:06:40 +0200

    Added countHello function to 1_Funkcje_czasu

---
+++ 1_Zadania/obowiązkowe/1_Funkcje_czasu/js/app.js
@@ -0,0 +1,12 @@
+function countHello(number){
+    var counter = 1;
+var interval = setInterval(() =>{
+    if(counter == number){
+        clearInterval(interval)
+    }
+console.log("Hello", counter);
+counter++;
+}, 500)
+}
+
+countHello(5);

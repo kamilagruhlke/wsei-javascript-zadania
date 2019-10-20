8db6b7f5cd8d85dcbd7fcb52b133b8b6653873bf
kamilagruhlke <kamilagruhlke@gmail.com>
2019-10-20 11:52:07 +0200

    Added solution for 1_Zadania

--- 1_Zadania/obowiązkowe/3_Tablice_wielowymiarowe/js/app.js
+++ 1_Zadania/obowiązkowe/3_Tablice_wielowymiarowe/js/app.js
@@ -1,4 +1,4 @@
-
+//Zadanie 1
 var task1Array = [
     [2, 3],
     ["Ala", "Ola"],
@@ -7,9 +7,41 @@ var task1Array = [
     [12, 15, 67]
 ];
 
+console.log(task1Array[3][2]);
+console.log(task1Array[2].length);
+console.log(task1Array[4][2]);
 
+//Zadanie 2
 var task2Array = [
     [1, 2, 3, 4],
     [5, 6, 7, 8],
     [9, 10, 11, 12]
 ];
+
+//Zadanie 3
+function print2DArray(tab){
+    for(let i in tab){
+        for(let j in tab[i]){
+            console.log(tab[i][j])
+        }
+    }
+}
+print2DArray([
+    [1,2,3],
+    [4,5,6]
+])
+
+//Zadanie 4
+var newtab2 = [
+    [1,2,3],
+    ["a", "b"]
+]
+
+function print2DArray(tab){
+    for(let i in tab){
+        for(let j in tab[i]){
+            console.log(tab[i][j])
+        }
+    }
+}
+print2DArray(newtab2);
